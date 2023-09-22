22f89644f593c2af78a22df77f3d3927bf02b1a2
Jishap-Tomsher <jishap.tomsher@gmail.com>
2023-09-22 12:06:12 +0400

    our heritage page

---
+++ resources/views/admin/pages/heritage.blade.php
@@ -0,0 +1,194 @@
+@extends('layouts.admin.app', ['body_class' => 'nav-md', 'title' => 'Our Heritage Page Contents'])
+@section('content')
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col-12">
+                <h1>Our Heritage Page Contents</h1>
+                <div class="separator mb-5"></div>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-8 offset-2">
+
+                <x-status />
+
+                <div class="card mb-4">
+                    <div class="card-body">
+                        <form class="repeater" method="POST"
+                            action="{{ route('admin.page.store-heritage', [
+                                'page_name' => 'heritage',
+                            ]) }}"
+                            enctype="multipart/form-data">
+                            @csrf
+                            <div class="form-group">
+                                <label for="exampleInputEmail1">Banner Title</label>
+                                <input type="text" name="title" class="form-control"
+                                    value="{{ old('title', $data->getTranslation('title','en') ?? '') }}" >
+                                <x-input-error name='title' />
+                            </div>
+                            <div class="form-group">
+                                <label for="exampleInputEmail1">Arabic Banner Title</label>
+                                <input type="text" name="ar_title" dir="rtl" class="form-control"
+                                    value="{{ old('ar_title', $data->getTranslation('title', 'ar')) }}" >
+                                <x-input-error name='ar_title' />
+                            </div>
+
+                            <div class="form-group">
+                                <label for="exampleInputEmail1">Banner Image</label>
+                                <div class="input-group mb-3">
+                                    <div class="custom-file">
+                                        <input name="image1" class="img" type="file" class="custom-file-input"
+                                            id="image1" accept="image/*">
+                                        <label class="custom-file-label" for="image1">Choose
+                                            file</label>
+                                    </div>
+                                </div>
+                                <x-input-error name='image1' />
+                            </div>
+
+                            <div class="form-group">
+                                <label for="exampleInputEmail1">Current Banner Image</label>
+                                <img class="img-custom form-control" src="{{ $data->getImage1() }}" alt="">
+                            </div>
+
+                            <div class="form-group position-relative error-l-50">
+                                <label>Content</label>
+                                <textarea class="form-control" id="engContent" name="content" rows="2">{{ old('content', $data->getTranslation('content1','en')) }}</textarea>
+                                <x-input-error name='content' />
+                            </div>
+                            <div class="form-group position-relative error-l-50">
+                                <label>Arabic Content</label>
+                                <textarea class="form-control" id="arContent" dir="rtl" name="ar_content" rows="2">{{ old('ar_content', $data->getTranslation('content1', 'ar')) }}</textarea>
+                                <x-input-error name='ar_content' />
+                            </div>
+
+                            <div class="form-group">
+                                <label for="exampleInputEmail1">Big Image</label>
+                                <div class="input-group mb-3">
+                                    <div class="custom-file">
+                                        <input name="image2" class="img" type="file" class="custom-file-input"
+                                            id="image2" accept="image/*">
+                                        <label class="custom-file-label" for="image2">Choose
+                                            file</label>
+                                    </div>
+                                </div>
+                                <x-input-error name='image2' />
+                            </div>
+
+                            <div class="form-group">
+                                <label for="exampleInputEmail1">Current Big Image</label>
+                                <img class="img-custom form-control" src="{{ $data->getImage2() }}" alt="">
+                            </div>
+
+                            <div class="form-group">
+                                <label for="exampleInputEmail1">Small Image</label>
+                                <div class="input-group mb-3">
+                                    <div class="custom-file">
+                                        <input name="image3" class="img" type="file" class="custom-file-input"
+                                            id="image3" accept="image/*">
+                                        <label class="custom-file-label" for="image3">Choose
+                                            file</label>
+                                    </div>
+                                </div>
+                                <x-input-error name='image3' />
+                            </div>
+
+                            <div class="form-group">
+                                <label for="exampleInputEmail1">Current Small Image</label>
+                                <img class="img-custom form-control" src="{{ $data->getImage3() }}" alt="">
+                            </div>
+
+                            <div class="form-group">
+                                <h4>Points List</h4>
+                            </div>
+
+                            <div data-repeater-list="heritage_list">
+                                <!-- Start: product body -->
+                                <div data-repeater-item class="mt-3 px-sm-40 px-20">
+                                    <div class="form-group">
+                                        <label for="exampleInputEmail1">Content</label>
+                                        <textarea class="form-control" id="heritage" name="heritage" rows="3" placeholder="Enter point" >{{ old('heritage') }}</textarea>
+                                    </div>
+                                    <div class="form-group">
+                                        <label for="exampleInputEmail1">Arabic Content</label>
+                                        <textarea class="form-control" id="ar_heritage" dir="rtl" name="ar_heritage" rows="3" placeholder="Enter point" >{{ old('heritage') }}</textarea>
+                                    </div>
+                                    <div class="text-right">
+                                        <input data-repeater-delete class="btn btn-danger" type="button" value="Delete" />
+                                    </div>
+                                </div>
+                                <!-- End: product body -->
+                            </div>
+                            <div class=" px-sm-40 px-20">
+                                <input data-repeater-create class="btn btn-info my-3" type="button" value="Add New Point" />
+                            </div>
+
+
+
+                            @include('admin.common.seo_form')
+
+                            <button type="submit" class="btn btn-primary mb-0">Update</button>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+@endsection
+
+
+@push('header')
+    <link rel="stylesheet" href="{{ adminAsset('css/vendor/select2.min.css') }}" />
+    <link rel="stylesheet" href="{{ adminAsset('css/vendor/select2-bootstrap.min.css') }}" />
+    <style>
+        .img-custom{
+            width: 350px !important;
+            height: 200px !important;
+        }
+    </style>
+@endpush
+@push('footer')
+    <script src="{{ adminAsset('js/vendor/select2.full.js') }}"></script>
+    <script src="{{ adminAsset('js/jquery.repeater.min.js') }}"></script>
+    <script>
+        $('.img').on('change', function() {
+            var text = 'Choose file';
+
+            if (this.files[0]) {
+                text = this.files[0].name
+            }
+            console.log($(this));
+            $(this).siblings('label').text(text)
+        });
+
+        $(document).ready(function() {
+            var $repeater = $('.repeater').repeater({
+                                initEmpty: false,
+                                show: function() {
+                                    $(this).slideDown();
+                                },
+                                hide: function(deleteElement) {
+                                    if (confirm('Are you sure you want to delete this element?')) {
+                                        $(this).slideUp(deleteElement);
+                                    }
+                                },
+                                isFirstItemUndeletable: false
+                            });
+            $data = {!! $lists !!}
+
+            $repeater.setList($data);
+        });
+    
+
+    </script>
+
+    <script src="{{ adminAsset('js/tinymce/tinymce.min.js') }}"></script>
+    <x-tiny-script :editors="[
+        ['id' => '#engContent1', 'dir' => 'ltr'],
+        ['id' => '#arContent1', 'dir' => 'rtl'],
+        ['id' => '#engContent', 'dir' => 'ltr'],
+        ['id' => '#arContent', 'dir' => 'rtl'],
+
+
+    ]" />
+@endpush
--- resources/views/frontend/our_heritage.blade.php
+++ resources/views/frontend/our_heritage.blade.php
@@ -3,13 +3,13 @@
     <!--Page content-->
     <div class="vlt-page-content">
 
-        <div class="vlt-page-title vlt-page-title--style-3 jarallax"><img class="jarallax-img" src="{{ asset('assets/img/banner01.webp') }}"
+        <div class="vlt-page-title vlt-page-title--style-3 jarallax"><img class="jarallax-img" src="{{ asset($page->getImage1('image1')) }}"
                 alt="" loading="lazy">
             <div class="vlt-page-title__overlay"></div><a class="vlt-page-title__scroll-to vlt-scroll-to" href="#content"><i
                     class="fa fa-arrow-down"></i></a>
             <div class="container">
                 <h1 class="vlt-page-title__title lax" data-lax-translate-y="0 0, vh (vw*0.25)"
-                    data-lax-opacity="1 1, (vh*0.5) 0" data-lax-anchor=".vlt-page-title">Our heritage
+                    data-lax-opacity="1 1, (vh*0.5) 0" data-lax-anchor=".vlt-page-title">{{ $page->getTranslation('title') }}
                 </h1>
             </div>
         </div>
@@ -27,7 +27,7 @@
                     <div class="col-md-5">
 
                         <div class="position-relative">
-                            <!--Particle image--><img class="vlt-particle" src="{{ asset('assets/img/h2.jpg') }}"
+                            <!--Particle image--><img class="vlt-particle" src="{{ asset($page->getImage3()) }}"
                                 data-animation-name="fadeInUp" alt=""
                                 style="position: absolute; top: -120px; right: -100px; max-width: 263px; --animate-delay: 1s;"
                                 loading="lazy">
@@ -35,7 +35,7 @@
                             <div class="vlt-simple-image">
                                 <div class="vlt-simple-image__mask" data-aos="image-mask-animation" data-aos-delay="0">
                                     <div class="inside"></div>
-                                </div><img src="{{ asset('assets/img/h1.jpg') }}" alt="" loading="lazy">
+                                </div><img src="{{ asset($page->getImage2()) }}" alt="" loading="lazy">
                             </div>
                         </div>
                         <div class="vlt-gap-60--sm"></div>
@@ -46,24 +46,18 @@
 
                             <div class="vlt-gap-40"></div>
                             <p><strong>
-                                Al-Douri Group is one of the institutions that keep pace with the approach of modernity and
-                                technological development while preserving its identity and its ancient history, which dates
-                                back to the eighteenth century. </strong></p>
+                                {!! $page->getTranslation('content1') !!} </strong></p>
                             <div class="vlt-gap-40"></div>
                             <div class="has-black-color">
                                 <!--Styled list-->
                                 <ul class="vlt-styled-list vlt-styled-list--style-3">
-                                    <li>The inspiring story of Al-Douri began in the United Arab Emirates in 1979 with the
-                                        establishment of Al-Douri Company and Factory for Foodstuff Trading.
-                                    </li>
-                                    <li>Al Douri Group's first retail store was established in 1986 under the name Al Douri
-                                        Roastery. After that in 2006, Al Douri Mart Roastery and Supermarket was
-                                        established. In 2011</li>
-                                    <li> Al-Douri Group witnessed a major shift when it opened its fully automated factory
-                                        equipped with the latest machines and modern technology. In 2016
-                                    </li>
-                                    <li>Al Douri factory in Dubai obtained HACCP and ISO 22000:2005 quality certifications.
-                                    </li>
+                                    @if(!empty($heritageLists))
+                                        @foreach($heritageLists as $list)
+                                            <li>The inspiring story of Al-Douri began in the United Arab Emirates in 1979 with the
+                                                establishment of Al-Douri Company and Factory for Foodstuff Trading.
+                                            </li>
+                                        @endforeach
+                                    @endif
                                 </ul>
                             </div>
                             <div class="vlt-gap-50"></div>
