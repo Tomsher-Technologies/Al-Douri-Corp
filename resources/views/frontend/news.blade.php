eb6e0e0ad8b3a6f843acee578e57d3e7abf61acc
Jishap-Tomsher <jishap.tomsher@gmail.com>
2023-09-22 15:57:10 +0400

    news

--- resources/views/frontend/news.blade.php
+++ resources/views/frontend/news.blade.php
@@ -5,8 +5,10 @@
         <!--Page title-->
         <div class="vlt-page-title vlt-page-title--style-3 jarallax"><img class="jarallax-img"
                 src="{{ asset($page->getImage1('image1')) }}" alt="" loading="lazy">
-            <div class="vlt-page-title__overlay"></div><a class="vlt-page-title__scroll-to vlt-scroll-to" href="#content"><i
-                    class="fa fa-arrow-down"></i></a>
+            <div class="vlt-page-title__overlay"></div>
+            <a class="vlt-page-title__scroll-to vlt-scroll-to" href="#content">
+                <i class="fa fa-arrow-down"></i>
+            </a>
             <div class="container">
                 <h1 class="vlt-page-title__title lax" data-lax-translate-y="0 0, vh (vw*0.25)"
                     data-lax-opacity="1 1, (vh*0.5) 0" data-lax-anchor=".vlt-page-title">{{ $page->getTranslation('title') }}
